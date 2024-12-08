3a141d65c7a6164759ce1205ad53da9f88aa618b
myh4832 <dbsghd4832@gmail.com>
2024-12-08 10:47:54 +0000

    update

--- dataset_utils.py
+++ dataset_utils.py
@@ -59,9 +59,7 @@ def files_in_directory(directory_path):
 
 def tensorToPIL(tensor: torch.Tensor) -> Image:
     tensor = tensor.clone()
-    tensor_min = tensor.min()
-    tensor_max = tensor.max()
-    tensor = (tensor - tensor_min) / (tensor_max - tensor_min)  # 정규화
+    tensor = tensor.div(2).add(0.5)
 
     return ToPILImage()(tensor)
 
